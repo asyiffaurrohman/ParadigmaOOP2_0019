d20fa41d97a6082c0a4f7414a345b7a3f78a245c
asyiffaurrohman <akungithub934@gmail.com>
2026-05-18 12:08:45 +0700

    tambah class Abstraksi dengan metod

---
+++ abstraksi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKls{
+    private: string x, y;
+    public:
+
+    //
+    //
+    void setXY(string a, string b){
+        x = a;
+        y = b;
+    }
+    //
+    void display(){
+        cout << "Nilai x: " << x << endl;
+        cout << "Nilai y: " << y << endl;
+    }
+};
+
