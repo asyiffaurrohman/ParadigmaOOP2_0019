442d57d25d24079d239439392a0f7b8cb3f4b7f3
asyiffaurrohman <akungithub934@gmail.com>
2026-05-18 13:00:21 +0700

    tambah class  dengan implementasi metode potongAdmin

---
+++ rekening_bank.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class RekeningBank {
+protected:
+    double saldo;
+
+public:
+    RekeningBank(double s) {
+        saldo = s;
+    }
+
+    virtual void potongAdmin() = 0;
+
+    void tampilSaldo() {
+        cout << "Saldo akhir: Rp " << saldo << endl;
+    }
+};
+
+class RekeningSyariah : public RekeningBank {
+public:
+    RekeningSyariah(double s) : RekeningBank(s) {}
+
+    void potongAdmin() {
+        cout << "Rekening Syariah bebas biaya admin" << endl;
+    }
+};
+
